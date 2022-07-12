3277e369dee4fc83f4d31d8fe1095813d2d6e411
Thresh <601426683@qq.com>
2022-07-12 22:47:43 +0800

    feat: init

---
+++ src/pages/AntdFormPage.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, Component } from 'react';
+import { Form, Button, Input } from 'antd';
+
+const FormItem = Form.Item;
+
+const nameRules = { required: true, message: '请输入姓名！' };
+const passworRules = { required: true, message: '请输入密码！' };
+
+export default class AntdFormPage extends Component {
+  formRef = React.createRef();
+
+  componentDidMount() {
+    this.formRef.current.setFieldsValue({ username: 'defalut' });
+  }
+
+  onFinish = (val) => {
+    console.log('onFinish', val);
+  };
+
+  onFinishFailed = (val) => {
+    console.log('onFinishFailed', val);
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>AntdFormPage</h3>
+        <Form
+          ref={this.formRef}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+        >
+          <FormItem name="username" label="姓名" rules={[nameRules]}>
+            <Input placeholder="username placeholder" />
+          </FormItem>
+          <FormItem name="password" label="密码" rules={[passworRules]}>
+            <Input placeholder="password placeholder" />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" size="large" htmlType="submit">
+              Submit
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
+}
+
+// Antd 3 Form HOC
+// todo antd4 form store (state、)
+
+// export default function AntdFormPage(props) {
+//   const [form] = Form.useForm();
+//   const onFinish = (val) => {
+//     console.log("onFinish", val);
+//   };
+//   const onFinishFailed = (val) => {
+//     console.log("onFinishFailed", val);
+//   };
+
+//   useEffect(() => {
+//     form.setFieldsValue({username: "defalut"});
+//     console.log("form", form);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h3>AntdFormPage</h3>
+//       <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+//         <FormItem name="username" label="姓名" rules={[nameRules]}>
+//           <Input placeholder="username placeholder" />
+//         </FormItem>
+//         <FormItem name="password" label="密码" rules={[passworRules]}>
+//           <Input placeholder="password placeholder" />
+//         </FormItem>
+//         <FormItem>
+//           <Button type="primary" size="large" htmlType="submit">
+//             Submit
+//           </Button>
+//         </FormItem>
+//       </Form>
+//     </div>
+//   );
+// }
