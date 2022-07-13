fe42eacb256b7def842834736f797da750b4a201
Thresh <601426683@qq.com>
2022-07-13 23:56:19 +0800

    feat: 实现Field函数组件版

--- src/components/my-rc-field-form/Field.jsx
+++ src/components/my-rc-field-form/Field.jsx
@@ -1,24 +1,59 @@
 import React, { Component } from 'react';
 import FieldContext from './FieldContext';
 
-export default class Field extends Component {
-  static contextType = FieldContext;
+// export default class Field extends Component {
+//   static contextType = FieldContext;
 
-  componentDidMount() {
-    this.unregister = this.context.registerFieldEntities(this);
-  }
+//   componentDidMount() {
+//     this.unregister = this.context.registerFieldEntities(this);
+//   }
 
-  componentWillUnmount() {
-    this.unregister();
-  }
+//   componentWillUnmount() {
+//     this.unregister();
+//   }
 
-  onStoreChange = () => {
-    this.forceUpdate();
-  };
+//   onStoreChange = () => {
+//     this.forceUpdate();
+//   };
 
-  getControlled = () => {
-    const { getFieldValue, setFieldsValue } = this.context;
-    const { name } = this.props;
+//   getControlled = () => {
+//     const { getFieldValue, setFieldsValue } = this.context;
+//     const { name } = this.props;
+//     return {
+//       value: getFieldValue(name), //"omg", // get state
+//       onChange: (e) => {
+//         const newValue = e.target.value;
+//         // set state
+//         setFieldsValue({ [name]: newValue });
+//       },
+//     };
+//   };
+
+//   render() {
+//     const { children } = this.props;
+
+//     const returnChildNode = React.cloneElement(children, this.getControlled());
+//     return returnChildNode;
+//   }
+// }
+
+export default function Field(props) {
+  const { children, name } = props;
+
+  const { getFieldValue, setFieldsValue, registerFieldEntities } =
+    React.useContext(FieldContext);
+
+  const [, forceUpdate] = React.useReducer((x) => x + 1, 0);
+
+  React.useLayoutEffect(() => {
+    const unregister = registerFieldEntities({
+      props,
+      onStoreChange: forceUpdate,
+    });
+    return unregister;
+  }, []);
+
+  const getControlled = () => {
     return {
       value: getFieldValue(name), //"omg", // get state
       onChange: (e) => {
@@ -29,10 +64,7 @@ export default class Field extends Component {
     };
   };
 
-  render() {
-    const { children } = this.props;
+  const returnChildNode = React.cloneElement(children, getControlled());
 
-    const returnChildNode = React.cloneElement(children, this.getControlled());
-    return returnChildNode;
-  }
+  return returnChildNode;
 }
